259cf55c2e1329118b2984dfe7483a724b5e2382
Sunggon Song <62205620+gon2gon2@users.noreply.github.com>
2022-04-29 16:53:16 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 apt update
-apt install -y git gcc make valgrind gcc-multilib
+apt install -y git gcc make valgrind gcc-multilib gdb
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 apt update
